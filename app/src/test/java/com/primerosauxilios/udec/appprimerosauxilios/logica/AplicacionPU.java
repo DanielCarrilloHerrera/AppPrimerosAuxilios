f23060d3281f4cff67e13c48ff7ae29bae938d46
Daniel Carrillo Herrera <danielcarrillohcse@gmail.com>
2017-11-18 08:09:00 -0500

    Modificada la clase aplicación de acuerdo a los resultados de las pruebas unitarias.

--- app/src/test/java/com/primerosauxilios/udec/appprimerosauxilios/logica/AplicacionPU.java
+++ app/src/test/java/com/primerosauxilios/udec/appprimerosauxilios/logica/AplicacionPU.java
@@ -3,91 +3,85 @@ package com.primerosauxilios.udec.appprimerosauxilios.logica;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.primerosauxilios.udec.appprimerosauxilios.persistencia.DatabasePAConstantes;
-import com.primerosauxilios.udec.appprimerosauxilios.persistencia.DatabasePAConstantes.EntryCasos;
 import com.primerosauxilios.udec.appprimerosauxilios.persistencia.PrimerosAuxiliosDBHelper;
+
+import org.junit.Test;
+
 import java.util.ArrayList;
 
-public class Aplicacion {
+import static org.junit.Assert.*;
+
+/**
+ * Created by daniel on 18/11/17.
+ */
+
+public class AplicacionPU {
     private static Aplicacion instancia;
     private PrimerosAuxiliosDBHelper admin;
     private Context context;
     private SQLiteDatabase db;
     private ArrayList<Caso> listaCasos = new ArrayList();
 
-    private Aplicacion(Context context) {
-        this.context = context;
-        this.admin = new PrimerosAuxiliosDBHelper(context);
-    }
 
-    public static Aplicacion getInstancia(Context context) {
-        if (instancia == null) {
-            instancia = new Aplicacion(context);
-        }
-        return instancia;
-    }
+    @Test
+    public void getListaCasos() throws Exception {}
 
-    public ArrayList<Caso> getListaCasos() {
-        return this.listaCasos;
-    }
-
-    public Caso getCaso(int id) {
-        return null;
-    }
-
-    public Caso getCaso(String nombre) {
+    @Test
+    public void getCaso() throws Exception {
         try {
             this.db = this.admin.getWritableDatabase();
         }
         catch(NullPointerException e){
-            return null;
+            return;
         }
 
+        String nombre="ejemplo";
         Caso retorno = new Caso();
         Cursor caso = this.db.rawQuery("SELECT _id, " +
-                "nombre, " +
-                "procedimiento, " +
-                "clavesBusqueda, " +
-                "archivoAudio " +
-                "FROM Casos " +
-                "WHERE nombre " +
-                "LIKE '%" + nombre + "%' ", null);
+                                        "nombre, " +
+                                        "procedimiento, " +
+                                        "clavesBusqueda, " +
+                                        "archivoAudio " +
+                                        "FROM Casos " +
+                                        "WHERE nombre " +
+                                        "LIKE '%" + nombre + "%' ", null);
         caso.moveToFirst();
         retorno.setId(caso.getInt(0));
         retorno.setNombre(caso.getString(1));
         retorno.asignarProcedimiento(caso.getString(2));
         retorno.agregarPalabraClave(caso.getString(3));
         retorno.asignarAudioAProcedimiento(caso.getString(4));
-        return retorno;
     }
 
-    public ArrayList<String> getNombresCasos(String palabrasClave) {
+
+    @Test
+    public void getNombresCasos() throws Exception {
         try{
             this.db = this.admin.getWritableDatabase();
         }catch (NullPointerException e){
-            return null;
+            return;
         }
+        String palabrasClave = "ejemplo";
         ArrayList<String> nombreCasos = new ArrayList();
-        Cursor listaDeCasos = this.db.rawQuery("SELECT nombre " +
-                                                "FROM Casos " +
-                                                "WHERE clavesBusqueda " +
-                                                "LIKE '%" + palabrasClave.toLowerCase() +
-                                                "%'", null);
+        Cursor listaDeCasos = this.db.rawQuery("SELECT nombre FROM Casos WHERE clavesBusqueda LIKE '%" + palabrasClave.toLowerCase() + "%'", null);
         listaDeCasos.moveToFirst();
         while (!listaDeCasos.isAfterLast()) {
             nombreCasos.add(listaDeCasos.getString(listaDeCasos.getColumnIndex(DatabasePAConstantes.EntryCasos.NOMBRE)));
             listaDeCasos.moveToNext();
         }
         listaDeCasos.close();
-        return nombreCasos;
     }
 
-    public String agregarNuevoCaso(String nombreCaso) {
-        return null;
+    @Test
+    public void agregarNuevoCaso() throws Exception {
     }
 
-    public String eliminarCaso(String nombreCaso) {
-        return null;
+    @Test
+    public void eliminarCaso() throws Exception {
     }
+
 }
